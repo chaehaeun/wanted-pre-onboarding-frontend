18acad501baa7d0c0af115e8e66f61bfe81e98c1
chaehaeun <chae_3210@naver.com>
2023-07-31 22:35:50 +0900

    [feat] Error 페이지 카운팅 후 이전 페이지로 이동

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,7 +1,30 @@
+import { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+
 const Error = () => {
+  const navigate = useNavigate()
+  const [count, setCount] = useState(3)
+
+  useEffect(() => {
+    const countdownInterval = setInterval(() => {
+      setCount(prevCount => prevCount - 1)
+    }, 1000)
+
+    return () => {
+      clearInterval(countdownInterval)
+    }
+  }, [])
+
+  useEffect(() => {
+    if (count === 0) {
+      navigate(-1)
+    }
+  }, [count, navigate])
+
   return (
     <div>
       <h2>page not found!</h2>
+      <p>{count}초 뒤 이전 페이지로 이동합니다.</p>
     </div>
   )
 }
