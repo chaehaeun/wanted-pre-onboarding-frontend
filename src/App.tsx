2b4dd37517046a129a9d01a09ef19c7734008a1f
chaehaeun <chae_3210@naver.com>
2023-07-31 21:20:37 +0900

    [setting] 초기 환경 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import SignIn from 'components/SignIn'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: (
+      <div>
+        <p>home</p>
+      </div>
+    ),
+  },
+  {
+    path: '/signin',
+    element: <SignIn />,
+  },
+])
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <RouterProvider router={router} />
 }
 
-export default App;
+export default App
---
+++ src/components/SignIn.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const SignIn = () => {
+  return <div>Fine</div>
+}
+
+export default SignIn
