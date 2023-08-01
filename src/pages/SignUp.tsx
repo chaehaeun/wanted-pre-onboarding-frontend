f90ea99e100392103ad7d2210e1824836f175092
chaehaeun <chae_3210@naver.com>
2023-08-01 22:32:40 +0900

    [feat] SignUp 페이지 마크업 및 스타일링

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import { AuthForm, AuthInput } from 'components'
+import Title from 'components/Title/Title'
 
 const SignUp = () => {
-  return <div>signup page!</div>
+  return (
+    <div>
+      <Title>회원가입</Title>
+      <AuthForm legend={'회원가입 폼'}>
+        <AuthInput
+          id={'signupEmail'}
+          label={'Email'}
+          placeholder={'이메일을 입력해주세요.'}
+          type={'email'}
+          dataTestId={'email-input'}
+          warning="이메일 형식이 올바르지 않습니다."
+        />
+        <AuthInput
+          id={'signupPassword'}
+          label={'Password'}
+          placeholder={'비밀번호를 입력해주세요.'}
+          type={'password'}
+          dataTestId={'password-input'}
+          warning="비밀번호는 8자 이상이어야 합니다."
+        />
+      </AuthForm>
+    </div>
+  )
 }
 
 export default SignUp
