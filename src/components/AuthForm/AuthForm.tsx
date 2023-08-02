62a54c7592b7dd791ec27952bfa64205a1b6dbed
chaehaeun <chae_3210@naver.com>
2023-08-02 22:40:37 +0900

    [feat] submit 함수 추가

--- src/components/AuthForm/AuthForm.tsx
+++ src/components/AuthForm/AuthForm.tsx
@@ -3,14 +3,25 @@ import React from 'react'
 interface AuthFormProps {
   legend: string
   children: React.ReactNode
+  type: 'signin' | 'signup'
 }
 
-const AuthForm = ({ legend, children }: AuthFormProps) => {
+const AuthForm = ({ legend, children, type }: AuthFormProps) => {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    if (type === 'signin') {
+      console.log('로그인 폼 제출')
+    }
+    if (type === 'signup') {
+      console.log('회원가입 폼 제출')
+    }
+  }
+
   return (
-    <form className="w-4/5 mx-auto ">
+    <form className="w-4/5 mx-auto" onSubmit={handleSubmit}>
       <fieldset>
         <legend className="sr-only">{legend}</legend>
-        <div className="flex flex-col gap-8">{children}</div>
+        <div className="flex flex-col gap-6">{children}</div>
       </fieldset>
     </form>
   )
