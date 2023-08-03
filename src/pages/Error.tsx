9571454d616edbe15d1f5c8989838c5205d4a1ef
chaehaeun <chae_3210@naver.com>
2023-08-03 10:36:57 +0900

    [styles] error 페이지 스타일 수정

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,3 +1,4 @@
+import { Header } from 'components'
 import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 
@@ -17,14 +18,17 @@ const Error = () => {
 
   useEffect(() => {
     if (count === 0) {
-      navigate(-1)
+      navigate('/')
     }
   }, [count, navigate])
 
   return (
     <div>
-      <h2>page not found!</h2>
-      <p>{count}초 뒤 이전 페이지로 이동합니다.</p>
+      <Header />
+      <div className="flex flex-col items-center w-full ">
+        <h2 className="mb-2 text-3xl font-bold mt-7">page not found!</h2>
+        <p>{count}초 뒤 메인 페이지로 이동합니다.</p>
+      </div>
     </div>
   )
 }
