6246eac573d5bf1f7f0d886f456e820700485328
chaehaeun <chae_3210@naver.com>
2023-08-03 22:06:34 +0900

    [feat] Todo 추가 기능 추가

--- src/components/Todos/TodoItem.tsx
+++ src/components/Todos/TodoItem.tsx
@@ -1,8 +1,18 @@
 import { Button } from 'components'
-import React, { useId } from 'react'
+import { useState } from 'react'
 
-const TodoItem = () => {
-  const id = useId()
+interface TodoItemProps {
+  id: string
+  isComplete: boolean
+  todo: string
+}
+
+const TodoItem = ({ id, isComplete, todo }: TodoItemProps) => {
+  const [isCompleted, setIsCompleted] = useState(isComplete)
+
+  const handleCheckBox = () => {
+    setIsCompleted(prev => !prev)
+  }
 
   return (
     <li className="flex items-center w-full px-2">
@@ -10,9 +20,11 @@ const TodoItem = () => {
         className="relative w-5 h-5 mr-3 bg-white border-2 border-black appearance-none cursor-pointer shadow-wrap-sm checked:bg-teal-500 after:absolute  checked:after:content-['✓'] after:w-5 after:h-5 after:block after:-top-1 after:left-0"
         type="checkbox"
         id={id}
+        onChange={handleCheckBox}
+        checked={isCompleted}
       />
       <label className="flex-grow mt-1" htmlFor={id}>
-        할 일
+        {todo}
       </label>
       <Button type="button" dataTestId="modify-button" mode="edit">
         수정
--- src/components/Todos/Todos.tsx
+++ src/components/Todos/Todos.tsx
@@ -1,10 +1,38 @@
 import { Button, TodoItem } from 'components'
-import React from 'react'
+import React, { useState } from 'react'
+
+interface Todo {
+  id: string
+  text: string
+  status: boolean
+}
 
 const Todos = () => {
+  const [todos, setTodos] = useState<Todo[]>([])
+  const [value, setValue] = useState('')
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value)
+  }
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault()
+    const trimmedValue = value.trim()
+    if (!trimmedValue) return
+
+    const newTodo: Todo = {
+      id: Date.now().toString(),
+      text: trimmedValue,
+      status: false,
+    }
+
+    setTodos([...todos, newTodo])
+    setValue('')
+  }
+
   return (
     <div className="w-4/5 mx-auto">
-      <form className="w-full">
+      <form className="w-full" onSubmit={handleSubmit}>
         <fieldset>
           <legend className="sr-only">Todo 입력폼</legend>
           <div className="flex w-full shadow-wrap">
@@ -12,17 +40,14 @@ const Todos = () => {
               할 일
             </label>
             <input
+              onChange={handleChange}
+              value={value}
               type="text"
               id="todo"
               placeholder="할 일을 추가해주세요."
               className="flex-grow px-2 py-2 border-2 border-black focus:outline-none"
             />
-            <Button
-              mode="add"
-              type="submit"
-              onClick={() => {}}
-              dataTestId={'new-todo-add-button'}
-            >
+            <Button mode="add" type="submit" dataTestId={'new-todo-add-button'}>
               추가
             </Button>
           </div>
@@ -30,21 +55,14 @@ const Todos = () => {
       </form>
 
       <ul className="flex flex-col w-full gap-4 overflow-y-scroll mt-7 max-h-96">
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
+        {todos.map(todo => (
+          <TodoItem
+            key={todo.id}
+            id={todo.id}
+            todo={todo.text}
+            isComplete={todo.status}
+          />
+        ))}
       </ul>
     </div>
   )
