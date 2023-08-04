5c1f2cd0729be7b10272b9a190e2f6d5db850b3e
chaehaeun <chae_3210@naver.com>
2023-08-04 17:46:20 +0900

    [edit] status 401 케이스 추가

--- src/utils/makeModalContent.ts
+++ src/utils/makeModalContent.ts
@@ -6,6 +6,9 @@ const makeModalContent = (type: string, status: number) => {
       case 400:
         content = '이미 등록된 사용자입니다!'
         break
+      case 401:
+        content = '이메일 또는 비밀번호가 일치하지 않습니다!'
+        break
       case 404:
         content = '등록되지 않은 회원입니다!'
         break
