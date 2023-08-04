38c181773311fd47fea8ac895f95a743658f2547
chaehaeun <chae_3210@naver.com>
2023-08-05 02:41:22 +0900

    [Bug] 무한렌더링 방지를 위한 useCallback 추가

--- src/hooks/useModal.tsx
+++ src/hooks/useModal.tsx
@@ -1,15 +1,15 @@
-import { useState } from 'react'
+import { useCallback, useState } from 'react'
 
 const useModal = (initialState = { showModal: false, content: '' }) => {
   const [modalState, setModalState] = useState(initialState)
 
-  const openModal = (content: string) => {
+  const openModal = useCallback((content: string) => {
     setModalState({ showModal: true, content })
-  }
+  }, [])
 
-  const closeModal = () => {
+  const closeModal = useCallback(() => {
     setModalState({ showModal: false, content: '' })
-  }
+  }, [])
 
   return { ...modalState, openModal, closeModal }
 }
