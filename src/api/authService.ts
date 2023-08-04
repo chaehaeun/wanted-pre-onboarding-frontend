552cd81ccb8e4d4d989f2f5cb0771968e8f3f3a8
chaehaeun <chae_3210@naver.com>
2023-08-04 16:59:17 +0900

    [edit] axios 인스턴스 default export 수정

---
+++ src/api/authService.ts
@@ -0,0 +1,36 @@
+import { axiosInstance } from 'api/index'
+
+interface SignUpData {
+  email: string
+  password: string
+}
+
+interface SignInData {
+  email: string
+  password: string
+}
+
+class AuthService {
+  async signUp(data: SignUpData): Promise<void> {
+    try {
+      await axiosInstance.post('/auth/signup', data)
+    } catch (error) {
+      console.error('SignUp Error:', error)
+      throw error
+    }
+  }
+
+  async signIn(data: SignInData): Promise<string> {
+    try {
+      const response = await axiosInstance.post('/auth/signin', data)
+
+      return response.data.access_token
+    } catch (error) {
+      console.error('SignIn Error:', error)
+      throw error
+    }
+  }
+}
+
+const authService = new AuthService()
+export default authService
