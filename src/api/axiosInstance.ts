b5835c65e51dac5bbbf7d8c2b938a0e19ba305c2
chaehaeun <chae_3210@naver.com>
2023-08-04 17:03:13 +0900

    [edit] axios 인스턴스 default export 수정

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,8 +1,10 @@
 import axios from 'axios'
 
-export const axiosInstance = axios.create({
+const axiosInstance = axios.create({
   baseURL: process.env.REACT_APP_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
   },
 })
+
+export default axiosInstance
