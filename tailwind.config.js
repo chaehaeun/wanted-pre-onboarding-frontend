fac75f6a9af6d8aab05400a39becaf2408c2f343
chaehaeun <chae_3210@naver.com>
2023-08-04 17:08:25 +0900

    [setting] 백드롭 배경색, 박스쉐도우 설정 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,12 @@ module.exports = {
       fontFamily: {
         sans: ['Pretendard Variable', 'Pretendard', 'system-ui'],
       },
+      colors: {
+        modalBackDrop: 'rgba(0, 0, 0, 0.5)',
+      },
       boxShadow: {
         wrap: '4px 4px #000',
+        'wrap-sm': '2px 2px #000',
       },
     },
   },
