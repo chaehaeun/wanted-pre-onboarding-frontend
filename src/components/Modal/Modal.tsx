64e27fec60f47ada5f1ef34c3c13c09dfa41d652
chaehaeun <chae_3210@naver.com>
2023-08-04 13:58:15 +0900

    [feat] 재사용 가능한 모달 추가

---
+++ src/components/Modal/Modal.tsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+interface BackdropProps {
+  onClose: () => void
+}
+
+interface ModalOverlayProps {
+  children: React.ReactNode
+  onClose: () => void
+}
+
+interface ModalProps extends BackdropProps, ModalOverlayProps {}
+
+const Backdrop = ({ onClose }: BackdropProps) => {
+  return (
+    <div
+      className="fixed top-0 left-0 z-20 w-full h-screen bg-modalBackDrop"
+      onClick={onClose}
+    ></div>
+  )
+}
+
+const ModalOverlay = ({ children, onClose }: ModalOverlayProps) => {
+  return (
+    <div className="fixed z-30 -translate-x-1/2 -translate-y-1/2 bg-white border-2 border-black top-1/2 left-1/2 w-80 shadow-wrap">
+      <div className="w-full py-6 text-center">
+        <p>{children}</p>
+      </div>
+      <button
+        className="w-full py-4 transition-colors duration-200 ease-in bg-teal-500 border-t-2 border-black hover:bg-teal-600 focus:bg-teal-600 "
+        onClick={onClose}
+        type="button"
+      >
+        확인
+      </button>
+    </div>
+  )
+}
+
+const portalElement: HTMLElement | null = document.getElementById('overlays')
+
+const Modal = ({ onClose, children }: ModalProps) => {
+  if (portalElement) {
+    return (
+      <>
+        {ReactDOM.createPortal(<Backdrop onClose={onClose} />, portalElement)}
+        {ReactDOM.createPortal(
+          <ModalOverlay onClose={onClose}>{children}</ModalOverlay>,
+          portalElement,
+        )}
+      </>
+    )
+  }
+
+  return null
+}
+
+export default Modal
