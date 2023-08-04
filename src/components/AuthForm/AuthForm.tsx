a6043af1e284f072f2425f7cbf85abd8374527ad
chaehaeun <chae_3210@naver.com>
2023-08-04 17:08:47 +0900

    [feat] 로그인, 회원가입 통신 코드 추가

--- src/components/AuthForm/AuthForm.tsx
+++ src/components/AuthForm/AuthForm.tsx
@@ -1,19 +1,43 @@
+import { authService } from 'api'
+import Modal from 'components/Modal/Modal'
 import React from 'react'
+import { useModal } from 'hooks'
 
 interface AuthFormProps {
   legend: string
   children: React.ReactNode
   type: 'signin' | 'signup'
+  value: {
+    email: string
+    password: string
+  } | null
 }
 
-const AuthForm = ({ legend, children, type }: AuthFormProps) => {
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+const AuthForm = ({ legend, children, type, value }: AuthFormProps) => {
+  const { showModal, content, openModal, closeModal } = useModal()
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
     if (type === 'signin') {
-      console.log('로그인 폼 제출')
+      try {
+        if (value) {
+          const accessToken = await authService.signIn(value)
+          console.log('Access Token:', accessToken)
+          console.log('로그인 폼 제출 성공!')
+        }
+      } catch (error) {
+        console.error('SignIn Error:', error)
+      }
     }
     if (type === 'signup') {
-      console.log('회원가입 폼 제출')
+      try {
+        if (value) {
+          await authService.signUp(value)
+          console.log('회원가입 폼 제출 성공!')
+        }
+      } catch (error) {
+        console.error('SignUp Error:', error)
+      }
     }
   }
 
@@ -23,6 +47,7 @@ const AuthForm = ({ legend, children, type }: AuthFormProps) => {
         <legend className="sr-only">{legend}</legend>
         <div className="flex flex-col gap-6">{children}</div>
       </fieldset>
+      {showModal && <Modal onClose={closeModal}>{content}</Modal>}
     </form>
   )
 }
