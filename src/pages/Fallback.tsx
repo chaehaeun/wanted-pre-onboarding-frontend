37c1e382a06d5a36ad00fe9c41318f73b7425bea
chaehaeun <chae_3210@naver.com>
2023-08-24 20:44:37 +0900

    [styles] fallback 컴포넌트 생성

---
+++ src/pages/Fallback.tsx
@@ -0,0 +1,11 @@
+import { ClipLoader } from 'react-spinners'
+
+const Fallback = () => {
+  return (
+    <div className="flex items-center justify-center mt-44">
+      <ClipLoader />
+    </div>
+  )
+}
+
+export default Fallback
